e8ecc0c6f005d5c50219c57885cdf935e878d4d8
FRANCESCA SILVANO s282021 <s282021@studenti.polito.it>
2021-05-23 22:47:46 +0200

    repeated_combination v 1.1

--- WORK_ShyHLS/tcl_scripts/repeated_compination.tcl
+++ WORK_ShyHLS/tcl_scripts/repeated_compination.tcl
@@ -8,22 +8,52 @@
 #vett_fu vector of all fu
 
 proc repeated_comb {} {
+  source ./tcl_scripts/setenv.tcl
+  read_design ./data/DFGs/fir.dot
+  read_library ./data/RTL_libraries/RTL_library_multi-resources.txt
   set nodes [get_nodes]
+  set list_node 0
+  set vett_fu 0
   #preparation of the vett_index
+  #preparation of vett_fu with the fu that perform the operations of the dfg
   foreach element $nodes {
-    lappend vett_index  0
+    lappend vett_index  -1
+    lappend node_fu 0
+    #take from all the nodes all the operations
+    set node_operation [get_attribute $element operation]
+    #if lsearch dont match the operation return -1
+    #take all the fu that perform that operation and append to the vector vett_fu
+    if {[lsearch $list_node $node_operation]== -1} {
+      set vett_fu [concat $vett_fu  [get_lib_fus_from_op $node_operation]]
+      lappend list_node $node_operation
+      
+    }
   }
-  set vett_fu [get_lib_fus]
+  set vett_fu [lreplace $vett_fu 0 0]
+  puts $vett_fu
   set lung_fu [llength $vett_fu]
-  while {[lindex $vett_index end]<lung_fu} {
-    for {set i 0} {$i<[llength $nodes]} {incr $i} {
+  while {[lindex $vett_index end]< $lung_fu } {
+    for {set i 0} {$i<[llength $nodes]} {incr i} {
       set element [lindex $vett_index $i]
-      if {(($i != 0 && [lindex $vett_index $i+1] == $lung_fu)|| $i == 0) && ($element < $lung_fu)} {
-          incr $element
-          set vett_index [lreplace $vett_index $i $i $element]
-          #pruning
+      if {(($i != 0 && [lindex $vett_index [expr $i -1]] ==$lung_fu)|| $element == -1 || $i == 0) && ($element < $lung_fu)} {
+        #needs only the fu that made the same operation
+        incr element
+        while {$element < $lung_fu} {
+          set node_operation [get_attribute [lindex $nodes $i] operation]
+          set fu [lindex $vett_fu $element]
+          if {[lsearch [get_lib_fus_from_op $node_operation] $fu] != -1} {
+            set node_fu [lreplace $node_fu $i $i $fu]
+            set vett_index [lreplace $vett_index $i $i  $element]
+            break
+          }
+          incr element
+        }
+        set vett_index [lreplace $vett_index $i $i  $element]
+        #pruning
       }
     }
+    puts $node_fu 
+
     #verifica
   }
 }
