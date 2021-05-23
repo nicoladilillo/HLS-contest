01f4fd2c98432cacd64760d2b7d9c836761e9509
FRANCESCA SILVANO s282021 <s282021@studenti.polito.it>
2021-05-23 13:54:39 +0200

    combinational itarative

--- WORK_ShyHLS/tcl_scripts/repeated_compination.tcl
+++ WORK_ShyHLS/tcl_scripts/repeated_compination.tcl
@@ -1,21 +1,30 @@
 #dont need any attributes because the list of node and fu is global
 #source ./tcl_scripts/setenv.tcl
 #read_design ./data/DFGs/fir.dot
-#devo dare in pasto alla procedura 0 vettore_di_fu vettore_soluzione lunghezza_vettore_fu lunghezza_vettore soluz 0 0
-proc repeated_comb_recursive {posix vett_fu soluz lung_fu lung_sol start } {
-  if {$posix >= $lung_sol} {
-    for{set i 0} {$i<$lung_sol} {incr $i} {
-        #setto vettore da verificare
+#nodes vector of nodes
+#vett_index lung= lung nodes 
+#soluz vector of temporary solution
+#better_soluz the best of the solution
+#vett_fu vector of all fu
+
+proc repeated_comb {} {
+  set nodes [get_nodes]
+  #preparation of the vett_index
+  foreach element $nodes {
+    lappend vett_index  0
+  }
+  set vett_fu [get_lib_fus]
+  set lung_fu [llength $vett_fu]
+  while {[lindex $vett_index end]<lung_fu} {
+    for {set i 0} {$i<[llength $nodes]} {incr $i} {
+      set element [lindex $vett_index $i]
+      if {(($i != 0 && [lindex $vett_index $i+1] == $lung_fu)|| $i == 0) && ($element < $lung_fu)} {
+          incr $element
+          set vett_index [lreplace $vett_index $i $i $element]
+          #pruning
+      }
     }
     #verifica
-    return
-  }
-  for {set i $start} {$i< $lung_fu} {incr i} {
-    #verifico che l'op di fu sia compatibile con l'op del nodo
-    set $sol($posix) $val($i)
-    incr $posix
-    repeated_comb_recursive {$pos $val $sol $n $k $start }
-    incr $start
   }
 }
 #next step fare questa cosa per ogni operazione vettore di nodi a cui serve e vettore di fu dell'operazione
